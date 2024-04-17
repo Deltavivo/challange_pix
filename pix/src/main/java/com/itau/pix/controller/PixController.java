a259003cc304fdf15a726c1ddef7624c3e9ec3a1
Delfim Baltazar <delfim.baltazar@gmail.com>
2024-04-17 19:17:23 -0300

    Adjust controller and service

--- pix/src/main/java/com/itau/pix/controller/PixController.java
+++ pix/src/main/java/com/itau/pix/controller/PixController.java
@@ -1,9 +1,7 @@
 package com.itau.pix.controller;
 
-import com.itau.pix.dto.CreatePixDTO;
-import com.itau.pix.dto.DeletePixResponseDTO;
-import com.itau.pix.dto.SearchPixResponseDTO;
-import com.itau.pix.dto.UpdatePixResponseDTO;
+import com.itau.pix.dto.*;
+import com.itau.pix.repository.PixRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,53 +13,60 @@ import com.itau.pix.service.PixService;
 public class PixController {
 
     @Autowired
+    private PixRepository pixRepository;
+
+    @Autowired
     private PixService pixService;
 
     @PostMapping
-    public ResponseEntity registryKey(@RequestBody CreatePixDTO createPixDTO){
-        pixService.createPix(createPixDTO);
-        return new ResponseEntity<>(HttpStatus.OK);
+    public ResponseEntity<CreatePixResponseDTO> createPix(@RequestBody CreatePixRequestDTO pixDTO){
+        return new ResponseEntity<>(pixService.createPix(pixDTO), HttpStatus.CREATED);
     }
 
-    @PostMapping("/{id}")
-    public ResponseEntity<UpdatePixResponseDTO> updateKey(@PathVariable Long id){
-        return new ResponseEntity<>(pixService.updateKey(id),HttpStatus.OK);
+    @PutMapping("/{id}")
+    public ResponseEntity<UpdatePixResponseDTO> updatePix(@PathVariable String id, @RequestBody UpdatePixRequestDTO pixDTO){
+        return new ResponseEntity<>(pixService.updatePix(id,pixDTO), HttpStatus.OK);
     }
 
-    @DeleteMapping("/{id}")
-    public ResponseEntity<DeletePixResponseDTO> deleteKey(@PathVariable Long id){
-        return new ResponseEntity<DeletePixResponseDTO>(pixService.deleteKey(id),HttpStatus.OK);
-
+    @PutMapping("/inactive/{id}")
+    public ResponseEntity<DeletePixResponseDTO> inactivePix(@PathVariable String id){
+        return new ResponseEntity<>(pixService.deletePix(id),HttpStatus.OK);
     }
 
-    @GetMapping("/{id}")
+    @GetMapping("/findById/{id}")
     public ResponseEntity<SearchPixResponseDTO> findByID(@PathVariable String id){
-        return new ResponseEntity<SearchPixResponseDTO>(pixService.findByID(id),HttpStatus.OK);
+        return new ResponseEntity<>(pixService.findById(id),HttpStatus.OK);
     }
 
-    @GetMapping("/{keyType}")
-    public ResponseEntity<SearchPixResponseDTO> findByKeyType(@PathVariable String keyType){
-        return new ResponseEntity<SearchPixResponseDTO>(pixService.findByKeyType(keyType),HttpStatus.OK);
-    }
+//    @GetMapping("/findByKeyType/{keyType}")
+//    public ResponseEntity<List<SearchPixResponseDTO>> findByKeyType(@PathVariable String keyType){
+//        return new ResponseEntity<List<SearchPixResponseDTO>>(pixService.findByKeyType(keyType),HttpStatus.OK);
+//
+//        try{
+//
+//        }catch(Exception ex){
+//            return new ResponseEntity<>(HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
 
-    @GetMapping("/{agencyAndAccount}")
-    public ResponseEntity<SearchPixResponseDTO> findByAgencyAndAccount(@PathVariable String agencyAndAccount){
-        return new ResponseEntity<SearchPixResponseDTO>(pixService.findByAgencyAndAccount(agencyAndAccount),HttpStatus.OK);
-    }
+//    @GetMapping("/findByAgencyAndAccount/{agencyAndAccount}")
+//    public ResponseEntity<SearchPixResponseDTO> findByAgencyAndAccount(@PathVariable String agencyAndAccount){
+//        return new ResponseEntity<SearchPixResponseDTO>(pixService.findByAgencyAndAccount(agencyAndAccount),HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/findByAccountHolderName/{accountHolderName}")
+//    public ResponseEntity<SearchPixResponseDTO> findByAccountHolderName(@PathVariable String accountHolderName ){
+//        return new ResponseEntity<SearchPixResponseDTO>(pixService.findByAccountHolderName(accountHolderName),HttpStatus.OK);
+//    }
+//
+//    @GetMapping("/findByKeyInclusionDate/{keyInclusionDate}")
+//    public ResponseEntity<SearchPixResponseDTO> findByKeyInclusionDate(@PathVariable String keyInclusionDate){
+//        return new ResponseEntity<SearchPixResponseDTO>(pixService.findByKeyInclusionDate(keyInclusionDate),HttpStatus.OK);
+//    }
 
-    @GetMapping("/{accountHolderName}")
-    public ResponseEntity<SearchPixResponseDTO> findByAccountHolderName(@PathVariable String accountHolderName ){
-        return new ResponseEntity<SearchPixResponseDTO>(pixService.findByAccountHolderName(accountHolderName),HttpStatus.OK);
-    }
-
-    @GetMapping("/{keyInclusionDate}")
-    public ResponseEntity<SearchPixResponseDTO> findByKeyInclusionDate(@PathVariable String keyInclusionDate){
-        return new ResponseEntity<SearchPixResponseDTO>(pixService.findByKeyInclusionDate(keyInclusionDate),HttpStatus.OK);
-    }
-
-    @GetMapping("/{keyInactivationDate}")
-    public ResponseEntity<SearchPixResponseDTO> findByKeyInactivationDate(@PathVariable String keyInactivationDate){
-        return new ResponseEntity<SearchPixResponseDTO>(pixService.findByKeyInactivationDate(keyInactivationDate),HttpStatus.OK);
-    }
+//    @GetMapping("/findByKeyInactivationDate/{keyInactivationDate}")
+//    public ResponseEntity<List<SearchPixResponseDTO>> findByKeyInactivationDate(@PathVariable String keyInactivationDate){
+//        return new ResponseEntity<List<SearchPixResponseDTO>>(pixService.findByKeyInactivationDate(keyInactivationDate),HttpStatus.OK);
+//    }
 
 }
--- pix/src/main/java/com/itau/pix/service/PixService.java
+++ pix/src/main/java/com/itau/pix/service/PixService.java
@@ -1,46 +1,184 @@
 package com.itau.pix.service;
 
-import com.itau.pix.dto.CreatePixDTO;
-import com.itau.pix.dto.DeletePixResponseDTO;
-import com.itau.pix.dto.SearchPixResponseDTO;
-import com.itau.pix.dto.UpdatePixResponseDTO;
+import com.itau.pix.dto.*;
+import com.itau.pix.entities.Pix;
+import com.itau.pix.repository.PixRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Calendar;
+import java.util.Optional;
+import java.util.UUID;
+import java.util.logging.Logger;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 @Service
 public class PixService {
-    public void createPix(CreatePixDTO createPixDTO) {
+
+    @Autowired
+    private PixRepository pixRepository;
+
+    public CreatePixResponseDTO createPix(CreatePixRequestDTO pixDTO) {
+
+        //TODO:verify if the key exist
+
+        Pix pix = Pix.builder()
+                .keyType(pixDTO.getKeyType())
+                .keyValue(pixDTO.getKeyValue())
+                .accountType(pixDTO.getAccountType())
+                .account(pixDTO.getAccount())
+                .agency(pixDTO.getAgency())
+                .accountHolderName(pixDTO.getAccountHolderName())
+                .accountHolderSurname(pixDTO.getAccountHolderSurname())
+                .dateTimeKeyIncluded(Calendar.getInstance().getTime()).build();
+
+        //save in database
+        Pix pixSaved = pixRepository.save(pix);
+
+        //TODO:verify if is valid
+        return CreatePixResponseDTO.builder().id(pixSaved.getId()).build();
+
+        //TODO:verify if is CPF or CNPJ and the limit to create
 
     }
 
-    public UpdatePixResponseDTO updateKey(Long id) {
+    public SearchPixResponseDTO findById(String id){
+
+        Optional<Pix> pixData = pixRepository.findById(UUID.fromString(id));
+
+        if(pixData.isPresent()){
+            return SearchPixResponseDTO.builder()
+                    .id(pixData.get().getId())
+                    .accountType(pixData.get().getAccountType())
+                    .keyType(pixData.get().getKeyType())
+                    .keyValue(pixData.get().getKeyValue())
+                    .account(pixData.get().getAccount())
+                    .agency(pixData.get().getAgency())
+                    .accountHolderName(pixData.get().getAccountHolderName())
+                    .accountHolderSurname(pixData.get().getAccountHolderSurname())
+                    .dateTimeKeyIncluded(pixData.get().getDateTimeKeyIncluded())
+                    .dateTimeKeyInactivation(pixData.get().getDateTimeKeyInactivation()).build();
+
+        }
+
+        return new SearchPixResponseDTO();
+
+    }
+    public UpdatePixResponseDTO updatePix(String id, UpdatePixRequestDTO pixDTO) {
+        //verify if the key exist
+        Optional<Pix> pixData = pixRepository.findById(UUID.fromString(id));
+
+        //verify if the key is inactive
+        if(!(pixData.get().getDateTimeKeyInactivation() == null)){
+            return new UpdatePixResponseDTO();
+        }
+        //cant update id, type or value of key
+        if(pixData.isPresent()){
+            Pix updatePix = Pix.builder()
+                    .keyType(pixData.get().getKeyType())
+                    .keyValue(pixData.get().getKeyValue())
+                    .accountType(pixDTO.getAccountType())
+                    .account(pixDTO.getAccount())
+                    .agency(pixDTO.getAgency())
+                    .accountHolderName(pixDTO.getAccountHolderName())
+                    .accountHolderSurname(pixDTO.getAccountHolderSurname())
+                    .dateTimeKeyIncluded(Calendar.getInstance().getTime()).build();
+
+            pixRepository.save(updatePix);
+
+            return UpdatePixResponseDTO.builder()
+                    .id(updatePix.getId())
+                    .keyType(updatePix.getKeyType())
+                    .keyValue(updatePix.getKeyValue())
+                    .accountType(updatePix.getAccountType())
+                    .agency(updatePix.getAgency())
+                    .account(updatePix.getAccount())
+                    .accountHolderName(updatePix.getAccountHolderName())
+                    .accountHolderSurname(updatePix.getAccountHolderSurname())
+                    .dateTimeKeyIncluded(updatePix.getDateTimeKeyIncluded()).build();
+
+        }
+
         return new UpdatePixResponseDTO();
     }
 
-    public DeletePixResponseDTO deleteKey(Long id) {
+    public DeletePixResponseDTO deletePix(String id){
+
+        //verify if exist Pix
+        Optional<Pix> pixData = pixRepository.findById(UUID.fromString(id));
+
+        if(pixData.isPresent()){
+
+            //verify if pix was inactive
+            if(!(pixData.get().getDateTimeKeyInactivation() == null)){
+                return new DeletePixResponseDTO();
+            }
+
+            //update inactivate data
+            Pix updatedPix = pixData.get();
+            updatedPix.setDateTimeKeyInactivation(Calendar.getInstance().getTime());
+
+            //save in database
+            pixRepository.save(updatedPix);
+
+            //create response dto
+            return DeletePixResponseDTO.builder()
+                    .id(pixData.get().getId())
+                    .accountType(pixData.get().getAccountType())
+                    .keyType(pixData.get().getKeyType())
+                    .keyValue(pixData.get().getKeyValue())
+                    .account(pixData.get().getAccount())
+                    .agency(pixData.get().getAgency())
+                    .accountHolderName(pixData.get().getAccountHolderName())
+                    .accountHolderSurname(pixData.get().getAccountHolderSurname())
+                    .dateTimeKeyIncluded(pixData.get().getDateTimeKeyIncluded())
+                    .dateTimeKeyInactivation(Calendar.getInstance().getTime()).build();
+
+        }
         return new DeletePixResponseDTO();
     }
 
-    public SearchPixResponseDTO findByID(String id) {
-        return new SearchPixResponseDTO();
+
+    private static final String CELLPHONE_PATTERN = "(?:(?:\\+|00)55\\s?)?(?:\\(?([1-9][0-9])\\)?\\s?)?(?:((?:9\\d|[2-9])\\d{3})?(\\d{4}))$";
+    private static final String EMAIL_PATTERN = "[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
+    private static final String CPF_PATTERN = "/^\\d{11}$";
+    private static final String CNPJ_PATTERN = "/^\\d{14}$";
+    private static final String RANDOM_PATTERN = "/^[a-z][a-zA-Z0-9]{36}";
+
+    private static final Pattern celPattern = Pattern.compile(CELLPHONE_PATTERN);
+    private static final Pattern emailPattern = Pattern.compile(EMAIL_PATTERN, Pattern.CASE_INSENSITIVE);
+    private static final Pattern cpfPattern = Pattern.compile(CPF_PATTERN);
+    private static final Pattern cnpjPattern = Pattern.compile(CNPJ_PATTERN);
+    private static final Pattern randomPattern = Pattern.compile(RANDOM_PATTERN);
+
+    public static boolean isValid(String value, String type ) {
+
+        switch (type) {
+            case "CELULAR":
+                Matcher celMatcher = celPattern.matcher(value);
+                return (celMatcher.matches());
+
+            case "EMAIL":
+                Matcher emailMatcher = emailPattern.matcher(value);
+                return (emailMatcher.matches());
+
+            case "CPF":
+                Matcher cpfMatcher = cpfPattern.matcher(value);
+                return (cpfMatcher.matches());
+
+            case "CNPJ":
+                Matcher cnpjMatcher = cnpjPattern.matcher(value);
+                return (cnpjMatcher.matches());
+
+            case "ALEATORIO":
+                Matcher randomMatcher = randomPattern.matcher(value);
+                return (randomMatcher.matches());
+
+            default:
+                return false;
+
+        }
     }
 
-    public SearchPixResponseDTO findByKeyType(String keyType) {
-        return new SearchPixResponseDTO();
-    }
-
-    public SearchPixResponseDTO findByAgencyAndAccount(String agencyAndAccount) {
-        return new SearchPixResponseDTO();
-    }
-
-    public SearchPixResponseDTO findByAccountHolderName(String accountHolderName) {
-        return new SearchPixResponseDTO();
-    }
-
-    public SearchPixResponseDTO findByKeyInclusionDate(String keyInclusionDate) {
-        return new SearchPixResponseDTO();
-    }
-
-    public SearchPixResponseDTO findByKeyInactivationDate(String keyInactivationDate) {
-        return new SearchPixResponseDTO();
-    }
 }
